ecfcaf85d36821563f484b5305430f627fbf52c8
Дворовенко Максим <144525592+snowaaaaaaaaaa@users.noreply.github.com>
2025-05-15 12:33:50 +0200

    Update api.php

--- api.php
+++ api.php
@@ -1,14 +1,21 @@
 <?php
 header('Content-Type: application/json');
+
+$host = 'books.mysql.database.azure.com';
+$dbname = 'books';
+$username = 'dvorovenko@books';
+$password = 'Premka228';
+
 try {
-    $db = new PDO('sqlite:books.db');
+    // Підключення до MySQL через PDO
+    $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $specialty = isset($_GET['specialty']) ? $_GET['specialty'] : null;
 
     if ($specialty) {
         $stmt = $db->prepare("SELECT * FROM books WHERE specialty = :specialty ORDER BY id DESC");
-        $stmt->bindParam(':specialty', $specialty, PDO::PARAM_STR);
+        $stmt->bindParam(':specialty', $specialty, PDO::PARAM_INT);
     } else {
         $stmt = $db->query("SELECT * FROM books ORDER BY id DESC");
     }
